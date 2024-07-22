a27c761ffc06c6e4fa2f63b41788b7160e29ef37
Jahdonah <jahdonah@Jahdonah-Pro.local>
2024-07-22 10:49:59 -0400

    Creating models and assign routes to it

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express =require('express');
+const path = require('path');
+const routes =require('./controllers');
 const sequelize=require('./config/connection');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// use routes
 
+app.use(routes);
 sequelize.sync({force:false}).then(()=>{
     app.listen(PORT,()=>console.log(`Now Listening on ${PORT}`));
 });
